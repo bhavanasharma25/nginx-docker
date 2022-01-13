519a08569f615252eac06b9722013dd1304ef158
Appinventiv <bhavna.sharma@appinventiv.com>
2022-01-13 13:44:14 +0530

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # nginx-docker
 create a 5 docker container and apply nginx for load balancing
+
+git clone whole project . and run a cmd docker-compose up
+in docker-compose file mysql is run on port 3306:3000 . we can't take 3000 bcoz we take above the 3306 port to run a container of mysql.
---
+++ app1/index.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World! app1");
+
+});
+
+var port = 5000;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
+
+
